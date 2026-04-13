91c7b6726b6506365169bb87f725c1ccb70d074b
YakovTamam <Yaakovt100@gmail.com>
2026-04-13 18:53:49 +0300

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    quietDeps: true, // This will silence deprecation warnings
+    quietDeps: true,
     silenceDeprecations: ["mixed-decls", "legacy-js-api"],
+  },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /odometer.*\.css$/,
+      use: "null-loader",
+    });
+
+    return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;

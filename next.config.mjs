814b3cda8727e0166162f40bbde8d6b6dfc93e06
YakovTamam <Yaakovt100@gmail.com>
2026-04-13 18:55:29 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
     quietDeps: true,
@@ -15,4 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
